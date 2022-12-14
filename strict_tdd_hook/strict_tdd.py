e4116a3a0779ddebc56876bf041ac90c87e8fff0
Benjamin Mummery <benjamin.mummery13@gmail.com>
2022-12-14 11:29:20 +0000

    feat: test parse mixed filenames

--- strict_tdd_hook/strict_tdd.py
+++ strict_tdd_hook/strict_tdd.py
@@ -5,13 +5,19 @@ from typing import List, Tuple
 def _construct_file_lists(
     filenames: List[str],
 ) -> Tuple[List[str], List[str], List[str]]:
-    if len(filenames) == 0:
-        return [], [], []
-    if filenames[0].startswith("src"):
-        return filenames, [], []
-    elif filenames[0].startswith("test"):
-        return [], filenames, []
-    return [], [], filenames
+    source_files: List[str] = []
+    test_files: List[str] = []
+    etc_files: List[str] = []
+
+    for file in filenames:
+        if file.startswith("src"):
+            source_files.append(file)
+        elif file.startswith("test"):
+            test_files.append(file)
+        else:
+            etc_files.append(file)
+
+    return source_files, test_files, etc_files
 
 
 def main(argv=None):
