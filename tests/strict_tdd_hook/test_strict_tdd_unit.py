535fc091b301a212f87e772cdc65ee8c5eef38da
Benjamin Mummery <benjamin.mummery13@gmail.com>
2022-12-14 10:28:59 +0000

    test: return empty lists if no filename

--- tests/strict_tdd_hook/test_strict_tdd_unit.py
+++ tests/strict_tdd_hook/test_strict_tdd_unit.py
@@ -1,1 +1,7 @@
 from strict_tdd_hook import strict_tdd
+
+
+class TestConstructFileLists:
+    @staticmethod
+    def test_empty_list():
+        assert ([], [], []) == strict_tdd._construct_file_lists([])
