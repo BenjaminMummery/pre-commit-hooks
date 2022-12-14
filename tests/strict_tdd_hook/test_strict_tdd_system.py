855de5b1e0bf29838bbae6cbc635dc01ccdc14fd
Benjamin Mummery <benjamin.mummery13@gmail.com>
2022-12-14 09:35:14 +0000

    test: fails for mix of text and metatext

--- tests/strict_tdd_hook/test_strict_tdd_system.py
+++ tests/strict_tdd_hook/test_strict_tdd_system.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import sys
 from contextlib import contextmanager
 
 import pytest
@@ -16,6 +15,14 @@ def cwd(path):
         os.chdir(oldcwd)
 
 
-def test_passes_for_no_changes(monkeypatch):
-    monkeypatch.setattr(sys, "argv", [])
-    subprocess.run(["strict-tdd"], check=True)
+class TestCLI:
+    @staticmethod
+    def test_passes_for_no_changes(tmp_path):
+        with cwd(tmp_path):
+            subprocess.run(["strict-tdd"], check=True)
+
+    @staticmethod
+    def test_fails_for_mix_of_text_and_metatext(tmp_path):
+        with cwd(tmp_path):
+            process = subprocess.run(["strict-tdd", "docs/foo.bar", "src/foo.bar"])
+        assert process.returncode == 1
