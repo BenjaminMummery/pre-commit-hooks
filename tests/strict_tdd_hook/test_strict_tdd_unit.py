e3c228b09f7250e97f794c8ef24db5a8adca11dd
Benjamin Mummery <benjamin.mummery13@gmail.com>
2022-12-14 10:34:05 +0000

    test: parse src filenames only.

--- tests/strict_tdd_hook/test_strict_tdd_unit.py
+++ tests/strict_tdd_hook/test_strict_tdd_unit.py
@@ -5,3 +5,8 @@ class TestConstructFileLists:
     @staticmethod
     def test_empty_list():
         assert ([], [], []) == strict_tdd._construct_file_lists([])
+
+    @staticmethod
+    def test_src_files_only():
+        filenames = ["src/foo", "src/bar"]
+        assert (filenames, [], []) == strict_tdd._construct_file_lists(filenames)
