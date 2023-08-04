67d9c3988dab18d0360bc6c8dd32983c9b247441
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-08-04 12:09:18 +0100

    style: add BDD.

--- tests/format_setup_cfg_hook/test_system_format_setup_cfg.py
+++ tests/format_setup_cfg_hook/test_system_format_setup_cfg.py
@@ -2,6 +2,9 @@
 
 """
 System tests for the format-setup-cfg hook.
+
+These tests duplicate the integration test cases, but enact them in a full end-to-end
+context.
 """
 
 import os
@@ -16,22 +19,27 @@ COMMAND = ["pre-commit", "try-repo", f"{os.getcwd()}", "format-setup-cfg"]
 class TestNoChanges:
     @staticmethod
     def test_no_files_changed(git_repo, cwd):
+        # WHEN
         with cwd(git_repo.workspace):
             process: subprocess.CompletedProcess = subprocess.run(COMMAND)
 
+        # THEN
         assert process.returncode == 0
 
     @staticmethod
     def test_no_supported_files_changed(git_repo, cwd):
+        # GIVEN
         files = ["hello.txt", ".gitignore", "test.yaml"]
         for file in files:
             f = git_repo.workspace / file
             f.write_text(f"<file {file} content sentinel>")
             git_repo.run(f"git add {file}")
 
+        # WHEN
         with cwd(git_repo.workspace):
             process: subprocess.CompletedProcess = subprocess.run(COMMAND)
 
+        # THEN
         assert process.returncode == 0
         for file in files:
             with open(git_repo.workspace / file, "r") as f:
