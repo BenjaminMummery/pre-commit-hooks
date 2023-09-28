53b6f1859565fd383109b4f88ca5a1f3dbd8855c
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-09-28 16:02:16 +0100

    feat: fail for bad rst.

--- src/check_docstrings_parse_as_rst_hook/check_docstrings_parse_as_rst.py
+++ src/check_docstrings_parse_as_rst_hook/check_docstrings_parse_as_rst.py
@@ -9,7 +9,80 @@ This module is intended for use as a pre-commit hook. For more information pleas
 consult the README file.
 """
 
+import argparse
+import re
+from pathlib import Path
+from typing import List
+
+from restructuredtext_lint import lint
+
+from src._shared import resolvers
+
+
+def _extract_docstrings(content: str) -> List[str]:
+    """
+    Find all the docstrings in the string.
+
+    Args:
+        content (str): the string to be checked.
+
+    Returns:
+        list(str): a list of docstrings found in the content.
+
+    """
+    return re.findall(r'"""([\s\S]*?)"""', content, re.M)
+
+
+def _check_docstring_rst(file: Path) -> int:
+    """
+    Identify docstrings in the file and check they are valid RST.
+
+    Args:
+        file (Path): the file to be examined
+
+    Returns:
+        int: 0 if all docstrings in the file are valid RST, 1 otherwise.
+    """
+    with open(file, "r") as f:
+        contents: str = f.read()
+
+    for docstring in _extract_docstrings(contents):
+        errors = lint(docstring)
+        if len(errors) > 0:
+            return 1
+    return 0
+
+
+def _parse_args() -> argparse.Namespace:
+    """
+    Parse the CLI arguments.
+
+    Returns:
+        argparse.Namespace with the following attributes:
+        - files (list of Path): the paths to each changed file relevant to this hook.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", nargs="*", default=[])
+    args = parser.parse_args()
+    args.files = resolvers._resolve_files(args.files)
+    return args
+
 
 def main() -> int:
     """Entrypoint for the check_docstrings_parse_as_rst hook."""
-    return 0
+    args = _parse_args()
+
+    # Early exit if no files provided
+    if len(args.files) < 1:
+        return 0
+
+    # Check the files
+    retv = 0
+    for file in args.files:
+        retv |= _check_docstring_rst(file)
+
+    return retv
+
+
+if __name__ == "__main__":
+    exit(main())
