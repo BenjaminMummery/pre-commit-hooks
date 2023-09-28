015d89e0e97d5d99ce60317af5b19dd616ce9707
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-09-28 13:57:03 +0100

    test: add remaining null-hypothesis system tests.

--- tests/check_docstrings_parse_as_rst_hook/test_system_check_docstrings_parse_as_rst.py
+++ tests/check_docstrings_parse_as_rst_hook/test_system_check_docstrings_parse_as_rst.py
@@ -16,3 +16,68 @@ class TestNoChanges:
             process: subprocess.CompletedProcess = subprocess.run(COMMAND)
 
         assert process.returncode == 0
+
+    @staticmethod
+    def test_no_supported_files_changed(git_repo, cwd):
+        files = ["hello.txt", ".gitignore", "test.yaml"]
+        for file in files:
+            f = git_repo.workspace / file
+            f.write_text(f"<file {file} content sentinel>")
+            git_repo.run(f"git add {file}")
+
+        with cwd(git_repo.workspace):
+            process: subprocess.CompletedProcess = subprocess.run(COMMAND)
+
+        assert process.returncode == 0
+        for file in files:
+            with open(git_repo.workspace / file, "r") as f:
+                content = f.read()
+            assert content == f"<file {file} content sentinel>"
+
+    @staticmethod
+    def test_no_changed_files_have_docstrings(git_repo, cwd):
+        # GIVEN
+        # create tracked but uncommitted files
+        files = ["hello.py", ".hello.py", "_hello.py"]
+        input_content = "# <file {file} content sentinel>\ndef main():\n    pass"
+        for file in files:
+            f = git_repo.workspace / file
+            f.write_text(input_content.format(file=file))
+            git_repo.run(f"git add {file}")
+
+        # WHEN
+        with cwd(git_repo.workspace):
+            process: subprocess.CompletedProcess = subprocess.run(COMMAND)
+
+        # THEN
+        assert process.returncode == 0
+        for file in files:
+            with open(git_repo.workspace / file, "r") as f:
+                output_content = f.read()
+            assert output_content == input_content.format(file=file)
+
+    @staticmethod
+    def test_all_docstrings_are_correct_rst(git_repo, cwd):
+        # GIVEN
+        files = ["hello.py", ".hello.py", "_hello.py"]
+        input_content = (
+            "# <file {file} content sentinel>\n"
+            "def main():\n"
+            '    """Valid rst."""'
+            "    pass"
+        )
+        for file in files:
+            f = git_repo.workspace / file
+            f.write_text(input_content.format(file=file))
+            git_repo.run(f"git add {file}")
+
+        # WHEN
+        with cwd(git_repo.workspace):
+            process: subprocess.CompletedProcess = subprocess.run(COMMAND)
+
+        # THEN
+        assert process.returncode == 0
+        for file in files:
+            with open(git_repo.workspace / file, "r") as f:
+                output_content = f.read()
+            assert output_content == input_content.format(file=file)
