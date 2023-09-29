b35056ba77fc15919d9728e603dabf4168f103b9
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-09-29 12:14:23 +0100

    refactor: cleaner test construction.

--- tests/check_docstrings_parse_as_rst_hook/test_integration_check_docstrings_parse_as_rst.py
+++ tests/check_docstrings_parse_as_rst_hook/test_integration_check_docstrings_parse_as_rst.py
@@ -1,10 +1,21 @@
 # Copyright (c) 2023 Benjamin Mummery
 
 from pathlib import Path
-
-import pytest
+from typing import List
 
 from src.check_docstrings_parse_as_rst_hook import check_docstrings_parse_as_rst
+
+
+def mock_file_content(
+    tmp_path: Path, files: List[str], input_content_filename: str, mocker
+):
+    with open(f"tests/examples/{input_content_filename}") as f:
+        input_content = f.read()
+    for file in files:
+        f = tmp_path / file
+        f.write_text(input_content)
+    mocker.patch("sys.argv", ["stub_name"] + files)
+    return input_content
 
 
 class TestNoChanges:
@@ -15,14 +26,10 @@ class TestNoChanges:
         assert check_docstrings_parse_as_rst.main() == 0
 
     @staticmethod
-    def test_changed_files_have_docstrings(mocker, cwd, tmp_path):
+    def test_no_changed_files_have_docstrings(mocker, cwd, tmp_path):
         # GIVEN
         files = ["hello.py", ".hello.py", "_hello.py"]
-        input_content = "# <file {file} content sentinel>\ndef main():\n    pass"
-        for file in files:
-            f = tmp_path / file
-            f.write_text(input_content.format(file=file))
-        mocker.patch("sys.argv", ["stub_name"] + files)
+        file_content = mock_file_content(tmp_path, files, "no_docstrings.py", mocker)
 
         # WHEN
         with cwd(tmp_path):
@@ -32,22 +39,13 @@ class TestNoChanges:
         for file in files:
             with open(tmp_path / file, "r") as f:
                 output_content = f.read()
-            assert output_content == input_content.format(file=file)
+            assert output_content == file_content
 
     @staticmethod
     def test_all_docstrings_are_correct_rst(mocker, cwd, tmp_path):
         # GIVEN
         files = ["hello.py", ".hello.py", "_hello.py"]
-        input_content = (
-            "# <file {file} content sentinel>\n"
-            "def main():\n"
-            '    """Valid rst."""'
-            "    pass"
-        )
-        for file in files:
-            f = tmp_path / file
-            f.write_text(input_content.format(file=file))
-        mocker.patch("sys.argv", ["stub_name"] + files)
+        file_content = mock_file_content(tmp_path, files, "valid_rst_python.py", mocker)
 
         # WHEN
         with cwd(tmp_path):
@@ -57,20 +55,24 @@ class TestNoChanges:
         for file in files:
             with open(tmp_path / file, "r") as f:
                 output_content = f.read()
-        assert output_content == input_content.format(file=file)
+        assert output_content == file_content
 
 
-@pytest.mark.parametrize("bad_rst", ["Underline too short\n========="])
 class TestBadRST:
     @staticmethod
-    def test_returns_1_for_single_bad_docstring(
-        mocker, cwd, tmp_path: Path, bad_rst: str
-    ):
+    def test_returns_1_for_single_bad_docstring(mocker, cwd, tmp_path: Path):
         # GIVEN
-        file = tmp_path / "test_file.py"
-        file.write_text(f'"""\n{bad_rst}\n"""')
-        mocker.patch("sys.argv", ["stub_name", "test_file.py"])
+        files = ["hello.py", ".hello.py", "_hello.py"]
+        file_content = mock_file_content(
+            tmp_path, files, "invalid_rst_python.py", mocker
+        )
 
         # WHEN
         with cwd(tmp_path):
             assert check_docstrings_parse_as_rst.main() == 1
+
+        # THEN
+        for file in files:
+            with open(tmp_path / file, "r") as f:
+                output_content = f.read()
+        assert output_content == file_content
---
+++ tests/examples/invalid_rst_python.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2023 Benjamin Mummery
+
+"""
+Module docstring.
+===============
+"""
+
+CONST = 3
+"""
+Const docstring.
+==============
+"""
+
+
+def main():
+    """
+    Function docstring.
+    ================
+    """
+    pass
+
+
+class foo:
+    """
+    Class docstring.
+    ==============
+    """
+
+    def method():
+        """
+        Method docstring.
+        ============
+        """
+        pass
+
+    class NestedClass:
+        """
+        Nested CLass docstring.
+        ==============
+        """
+
+        pass
+
+        def nested_method():
+            """
+            Nested Method docstring.
+            =================
+            """
+            pass
