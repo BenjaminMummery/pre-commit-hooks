b846e44077ef68a322eb2e5b5b914a9c7f95dd3b
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-10-05 11:17:49 +0100

    feat: excise previous hook

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,9 @@
 
 import os
 from contextlib import contextmanager
-from unittest.mock import Mock, create_autospec
+from unittest.mock import Mock
 
 import pytest
-
-import src
 
 
 @pytest.fixture(scope="session")
@@ -35,155 +33,6 @@ def mock_get_comment_markers(mocker):
 @pytest.fixture
 def mock_resolve_files(mocker):
     return mocker.patch("src._shared.resolvers._resolve_files")
-
-
-# endregion
-
-# region: add_copyright fixtures
-ADD_COPYRIGHT_FIXTURE_LIST = [
-    "mock_confirm_file_updated",
-    "mock_construct_copyright_string",
-    "mock_copyright_is_current",
-    "mock_default_config_file",
-    "mock_default_format",
-    "mock_ensure_comment",
-    "mock_ensure_copyright_string",
-    "mock_ensure_valid_format",
-    "mock_get_current_year",
-    "mock_get_earliest_commit_year",
-    "mock_get_git_user_name",
-    "mock_has_shebang",
-    "mock_infer_start_year",
-    "mock_insert_copyright_string",
-    "mock_parse_add_copyright_args",
-    "mock_parse_copyright_string",
-    "mock_parse_years",
-    "mock_ParsedCopyrightString",
-    "mock_ParsedCopyrightString_constructor",
-    "mock_read_config_file",
-    "mock_resolve_format",
-    "mock_resolve_user_name",
-    "mock_update_copyright_string",
-] + SHARED_FIXTURE_LIST
-
-
-@pytest.fixture
-def mock_confirm_file_updated(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._confirm_file_updated")
-
-
-@pytest.fixture
-def mock_construct_copyright_string(mocker):
-    return mocker.patch(
-        "src.add_copyright_hook.add_copyright._construct_copyright_string"
-    )
-
-
-@pytest.fixture
-def mock_copyright_is_current(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._copyright_is_current")
-
-
-@pytest.fixture
-def mock_default_config_file(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright.DEFAULT_CONFIG_FILE")
-
-
-@pytest.fixture
-def mock_default_format(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright.DEFAULT_FORMAT")
-
-
-@pytest.fixture
-def mock_ensure_comment(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._ensure_comment")
-
-
-@pytest.fixture
-def mock_ensure_copyright_string(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._ensure_copyright_string")
-
-
-@pytest.fixture
-def mock_ensure_valid_format(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._ensure_valid_format")
-
-
-@pytest.fixture
-def mock_get_current_year(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._get_current_year")
-
-
-@pytest.fixture
-def mock_get_earliest_commit_year(mocker):
-    return mocker.patch(
-        "src.add_copyright_hook.add_copyright._get_earliest_commit_year"
-    )
-
-
-@pytest.fixture
-def mock_get_git_user_name(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._get_git_user_name")
-
-
-@pytest.fixture
-def mock_has_shebang(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._has_shebang")
-
-
-@pytest.fixture
-def mock_infer_start_year(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._infer_start_year")
-
-
-@pytest.fixture
-def mock_insert_copyright_string(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._insert_copyright_string")
-
-
-@pytest.fixture
-def mock_parse_add_copyright_args(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._parse_args")
-
-
-@pytest.fixture
-def mock_parse_copyright_string(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._parse_copyright_string")
-
-
-@pytest.fixture
-def mock_parse_years(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._parse_years")
-
-
-@pytest.fixture
-def mock_read_config_file(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._read_config_file")
-
-
-@pytest.fixture
-def mock_resolve_format(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._resolve_format")
-
-
-@pytest.fixture
-def mock_resolve_user_name(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._resolve_user_name")
-
-
-@pytest.fixture
-def mock_update_copyright_string(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright._update_copyright_string")
-
-
-@pytest.fixture
-def mock_ParsedCopyrightString(mocker):
-    return create_autospec(src.add_copyright_hook.add_copyright.ParsedCopyrightString)
-
-
-@pytest.fixture
-def mock_ParsedCopyrightString_constructor(mocker):
-    return mocker.patch("src.add_copyright_hook.add_copyright.ParsedCopyrightString")
 
 
 # endregion
