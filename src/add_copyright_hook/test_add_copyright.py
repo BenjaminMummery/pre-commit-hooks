8e246ee467549fcae9894492f8ecb565f2bea112
Benjamin Mummery <benjamin.mummery13@gmail.com>
2024-04-08 13:28:35 +0100

    test: add test for no existing commits.

--- src/add_copyright_hook/test_add_copyright.py
+++ src/add_copyright_hook/test_add_copyright.py
@@ -6,17 +6,25 @@ import pytest
 from pytest_mock import MockerFixture
 
 from src.add_copyright_hook import add_copyright
-from src.add_copyright_hook.add_copyright import InvalidGitRepositoryError, Path, Repo
+from src.add_copyright_hook.add_copyright import (
+    GitCommandError,
+    InvalidGitRepositoryError,
+    NoCommitsError,
+    Path,
+    Repo,
+)
 
 
 @pytest.fixture()
 def mock_repo(mocker: MockerFixture) -> add_copyright.Repo:
-    return mocker.patch(f"{add_copyright.__name__}.Repo", create_autospec(Repo))
+    return mocker.patch(
+        f"{add_copyright.__name__}.Repo", Mock(return_value=create_autospec(Repo))
+    )
 
 
 class TestGetEarliestCommitYear:
     @staticmethod
-    def test_raises_InvalidGitRepositoryError_error_for_nonexistent_git_repo(
+    def test_raises_InvalidGitRepositoryError_for_nonexistent_git_repo(
         tmp_path: Path,
         cwd,
         mock_repo,
@@ -24,7 +32,23 @@ class TestGetEarliestCommitYear:
         # GIVEN
         mock_repo.side_effect = InvalidGitRepositoryError
 
-        # THEN
+        # WHEN / THEN
         with pytest.raises(InvalidGitRepositoryError):
+            with cwd(tmp_path):
+                add_copyright._get_earliest_commit_year(Mock())
+
+    @staticmethod
+    def test_raises_NoCommitsError_for_nonexistent_git_repo(
+        tmp_path: Path,
+        cwd,
+        mocker,
+    ):
+        # GIVEN
+        mocked_repo = create_autospec(Repo)
+        mocked_repo.blame.side_effect = GitCommandError(["<command sentinel>"])
+        mocker.patch(f"{add_copyright.__name__}.Repo", Mock(return_value=mocked_repo))
+
+        # WHEN / THEN
+        with pytest.raises(NoCommitsError):
             with cwd(tmp_path):
                 add_copyright._get_earliest_commit_year(Mock())
