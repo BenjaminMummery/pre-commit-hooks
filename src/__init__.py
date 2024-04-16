82ae87f5d51b22d291af5cb8456420864f13d686
Benjamin Mummery <benjamin.mummery13@gmail.com>
2024-04-16 15:57:51 +0100

    ci: automate release (#46)

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
-# Copyright (c) 2023 Benjamin Mummery
+# Copyright (c) 2023 - 2024 Benjamin Mummery
 
 """Initialiser to make pre-commit hooks accessible to externally."""
+
+# Version number, automatically updated by semantic-release
+__version__ = "2.0.0"
