99eca2b5af2c2046e428ee91dd674ad83eb16f0f
Benjamin Mummery <bmummery@psiquantum.com>
2025-07-02 14:56:22 +0100

    feat: extend words dict.

--- src/americanise_hook/americanise.py
+++ src/americanise_hook/americanise.py
@@ -16,10 +16,49 @@ from pathlib import Path
 from src._shared import resolvers
 
 DICTIONARY = {
+    # -se -> -ize
+    "characterise": "characterize",
     "initialise": "initialize",
     "instantiater": "instantiator",
-    "parametrise": "parametrise",
+    "parametrise": "parametrize",
+    "prioritise": "prioritize",
+    "specialise": "specialize",
+    "organise": "organize",
+    # -yse -> -yze
+    "analyse": "analyze",
+    "catalyse": "catalyze",
+    # -our -> -or
     "armour": "armor",
+    "behaviour": "behavior",
+    "colour": "color",
+    "flavour": "flavor",
+    "neighbour": "neighbor",
+    # -re -> -er
+    "centre": "center",
+    "fibre": "fiber",
+    "litre": "liter",
+    # -ae, -oe -> -e
+    "amoeba": "amoebae",
+    "anaesthesia": "anesthesia",
+    "caesium": "cesium",
+    # -ce -> -se
+    "defence": "defense",
+    "practise": "practice",  # british uses "practice" as the noun and "practise" as the verb. US uses "practice" for both.
+    "licence": "license",  # british uses "licence" as the noun and "license" as the verb. US uses "license" for both.
+    # -ge -> -g
+    "ageing": "aging",
+    "acknowledgement": "acknowledgment",
+    "judgement": "judgment",
+    # -ogue -> -og
+    "analogue": "analog",
+    "dialogue": "dialog",
+    # -l -> -ll
+    "fulfil": "fulfill",
+    "enrol": "enroll",
+    "skilful": "skillful",
+    # -ll -> -l
+    "labelled": "labeled",
+    "signalling": "signaling",
 }
 
 
